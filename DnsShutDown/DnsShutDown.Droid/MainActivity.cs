147aaedb2c2a6964f2a8c7f495fd22dbb64ea926
abdelkader <sancha@live.ca>
2016-12-18 11:29:11 -0500

    first commit

---
+++ DnsShutDown/DnsShutDown.Droid/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DnsShutDown.Droid
+{
+	[Activity (Label = "DnsShutDown.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+        ShutDowner shutdowner;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+            shutdowner = new ShutDowner();
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += async delegate {
+                try
+                {
+                    button.Enabled = false;
+                    
+
+                    await shutdowner.shutAsync();
+                    button.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    Toast.MakeText(this, "Error", ToastLength.Long).Show();
+                    button.Enabled = true;
+                }
+                
+            };
+		}
+	}
+}
+
+
---
+++ DnsShutDown/DnsShutDown/MyClass.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace DnsShutDown
+{
+    public class ShutDowner
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string ip { get; set; }
+
+        private string base_url;
+        private string shutdown_url;
+
+        public ShutDowner()
+        {
+            username = "admin";
+            password = "xxx";
+            ip = "192.168.0.154";
+
+            base_url = string.Format(@"http://{0}/goform/formLogin?f_login_type=0&f_LOGIN_NAME={1}&f_LOGIN_PASSWD={2}",
+                ip, username, password);
+
+            shutdown_url = string.Format(@"http://{0}/goform/sysShutDown?shootdown", ip);
+        }
+
+        public async Task shutAsync()
+        {
+            var client = new HttpClient();
+            await client.GetStringAsync(base_url);
+            await client.GetStringAsync(shutdown_url);
+
+        }
+    }
+
+}
+
